a68d75f6b86c743e9e67e01470d60e800c98abcd
Ruud Vriens <ruudvriens@gmail.com>
2016-12-30 01:09:18 +0100

    VS - Code fixes

--- src/DemoAngular2Web/gulpfile.js
+++ src/DemoAngular2Web/gulpfile.js
@@ -7,6 +7,11 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var merge = require('gulp-merge');
+var del = require('del');
+var tslint = require("gulp-tslint");
+var tsc = require("gulp-typescript");
+var Dotnet = require('gulp-dotnet');
+var pump = require('pump');
 
 
 gulp.task('default', function () {
@@ -18,10 +23,25 @@ paths.webroot = "wwwroot/";
 paths.npmSrc = "\./node_modules/";
 paths.npmLibs = paths.webroot + "lib/npmlibs/";
 
+paths.scripts =  ['Scripts/**/*.js', 'Scripts/**/*.ts', 'Scripts/**/*.map', 'Scripts/**/*.tsx'];
+var tsProject = tsc.createProject("Scripts/tsconfig.json");
+
 gulp.task("copy-deps:systemjs", function () {
     return gulp.src(paths.npmSrc + '/systemjs/dist/**/*.*', { base: paths.npmSrc + '/systemjs/dist/'})
          .pipe(gulp.dest(paths.npmLibs + '/systemjs/'));
 });
+
+
+gulp.task("clean", ["clean:js", "clean:dotnet"]);
+
+gulp.task('clean:js', function (cb) {
+    return del(['wwwroot/scripts/**/*']);
+});
+
+gulp.task('clean:dotnet', function (cb) {
+    return del(['bin/**/*']);
+});
+
 
 
 gulp.task("copy-deps:angular", function () {
@@ -42,7 +62,7 @@ gulp.task("copy-deps:angular", function () {
 
 gulp.task("copy-deps:react", function () {
 
-    var task = [
+    var tasks = [
         gulp.src(paths.npmSrc + '/react/dist/*.js', { base: paths.npmSrc + '/react/dist/' })
          .pipe(gulp.dest(paths.npmLibs + '/react/')),
         gulp.src(paths.npmSrc + '/react-dom/dist/*.js', { base: paths.npmSrc + '/react-dom/dist/' })
@@ -74,6 +94,17 @@ gulp.task("copy-deps:rxjs", function () {
          .pipe(gulp.dest(paths.npmLibs + '/rxjs/'));
 });
 
+gulp.task("copy-deps:jquery", function () {
+    return gulp.src(paths.npmSrc + '/jquery/dist/**/*.*', { base: paths.npmSrc + '/jquery/dist/' })
+         .pipe(gulp.dest(paths.npmLibs + '/jquery/dist/'));
+});
+
+gulp.task("copy-deps:bootstrap", function () {
+    return gulp.src(paths.npmSrc + '/bootstrap/dist/**/*.*', { base: paths.npmSrc + '/bootstrap/dist/' })
+         .pipe(gulp.dest(paths.npmLibs + '/bootstrap/dist/'));
+});
+
+
 gulp.task("copy-deps:zonejs", function () {
     return gulp.src(paths.npmSrc + '/zone.js/dist/**/*.js', { base: paths.npmSrc + '/zone.js/dist/' })
          .pipe(gulp.dest(paths.npmLibs + '/zone.js/'));
@@ -88,4 +119,26 @@ gulp.task("copy-deps:reflect-metadata", function () {
 
 
 // , 'copy-deps:es6-shim', 'copy-deps:es6-promise'
-gulp.task("copy-deps", ["copy-deps:rxjs", 'copy-deps:angular', 'copy-deps:react', 'copy-deps:systemjs', 'copy-deps:zonejs', 'copy-deps:reflect-metadata']);
+gulp.task("copy-deps", ["copy-deps:jquery", "copy-deps:bootstrap", "copy-deps:rxjs", 'copy-deps:angular', 'copy-deps:react', 'copy-deps:systemjs', 'copy-deps:zonejs', 'copy-deps:reflect-metadata']);
+
+
+gulp.task("build:dotnet", ["clean:dotnet"], function (cb) {
+    Dotnet.build({ cwd: './' }, cb);
+});
+
+gulp.task('build:js', ['clean:js'], function () { 
+    /*pump([
+        gulp.src(paths.scripts),
+        ,        
+        gulp.dest('wwwroot/scripts')
+    ],
+        cb
+    );*/
+    gutil.log('build: js');
+    return gulp.src(paths.scripts)
+            .pipe(tsProject()).js
+            .pipe(gulp.dest('wwwroot/app'));
+});
+
+
+gulp.task("build", ["build:dotnet", 'copy-deps', "build:js"]);
